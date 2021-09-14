28d3693575869d70a0cc87ccceb80436df96153b
srubey <scrubey@pdx.edu>
2021-09-14 14:34:52 -0600

    Updated filenames to work with docker

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def retirementcalculator():  # put application's code here
     return render_template('retirement-calculator.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
